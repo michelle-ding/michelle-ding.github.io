c3d0b04db81e18f56dc8e285e024c90372d6b99a
Michelle Ding <michelle_ding@brown.edu>
2023-12-12 19:05:36 -0500

    mobile note text edit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,9 @@ function App() {
         <Nav></Nav>
         <Body></Body>
       </div>
-      <div id="note"> <b>This website was designed and built by Michelle Ding. Last updated December 2023.</b></div>
+      <div id="note"> 
+      <b>This website was designed and built by Michelle Ding. Last updated December 2023.</b>
+      </div>
     </div>
     
   );
