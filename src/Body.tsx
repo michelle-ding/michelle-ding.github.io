043ed93332633d29b9bf0c963da205f9bd93d3b0
mding16 <michelle_ding@brown.edu>
2024-12-01 00:29:32 -0500

    ;

--- src/Body.tsx
+++ src/Body.tsx
@@ -336,9 +336,9 @@ function resources(){
                 <br></br>
             <a target="_blank" id="ref" href="https://docs.google.com/spreadsheets/d/1XL17SFOEy12K_yALJxL9mgCyD2IAn6MTO9eB6iyb05c/edit?usp=sharing">
                 * Crowdsourced resources for artists concerned about generative AI</a>
-                <br></br>
+                {/* <br></br>
                 <a target="_blank" id="ref" href="https://docs.google.com/spreadsheets/d/1eiZKnNeeQlvr4NaVOwCfhpNAaEr7lp-kELyvfjCZS64/edit?usp=sharing">
-                * Ways to turn off AI training on platforms</a>
+                * Ways to turn off AI training on platforms</a> */}
                 <br></br>
                 
                 <a target="_blank" id="ref" href="https://www.goodreads.com/review/list/181978928-michelle?order=d&shelf=michelles_sociotech_recs&sort=date_added">
