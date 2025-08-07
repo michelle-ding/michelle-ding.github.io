fdfc8746245b24af54162ba8ead9a252370b2382
mding16 <michelle_ding@brown.edu>
2025-08-06 19:25:16 -0700

    edit

--- src/Body.tsx
+++ src/Body.tsx
@@ -211,7 +211,7 @@ function publications(){
             Workshop (STAIG ’25), April 2025, Yokohama, Japan. <a id="ref" href="https://arxiv.org/abs/2504.17663" target="_blank">https://arxiv.org/abs/2504.17663</a>
             <br></br>
             <br></br>
-            <b>Michelle L. Ding</b> and Malika Saada Saar. "Silencing at Scale: AI-Generated Non-Consensual Intimate Images as a Threat to Technology Security."
+            Malika Saada Saar and <b>Michelle L. Ding</b>. "Silencing at Scale: AI-Generated Non-Consensual Intimate Images as a Threat to Technology Security."
             To be in the Oxford Handbook of Human Security in 2026.
         </div>
     )
