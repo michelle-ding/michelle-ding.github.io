2f16bbbeb8f181e30cea930e80d1b16566171d34
mding16 <michelle_ding@brown.edu>
2025-10-20 20:23:31 -0400

    edits

--- src/Research.tsx
+++ src/Research.tsx
@@ -8,12 +8,17 @@ export default function Research() {
       <ul className="publications-list">
       <li>
  <a id="ref" href="https://arxiv.org/abs/2504.17663" target="_blank">
-        The Malicious Technical Ecosystem: Exposing Limitations 
-        in Technical Governance of AI-Generated Nonconsensual Intimate Images of Adults.</a>
-        <br></br>
-       <span className="name">Michelle L. Ding</span> and Harini Suresh, 
+        "The Malicious Technical Ecosystem: Exposing Limitations 
+        in Technical Governance of AI-Generated Nonconsensual Intimate Images of Adults."</a>
+       <span className="name">Michelle L. Ding</span> and Harini Suresh. 
         Sociotechnical AI Governance
-        Workshop, CHI 2025       
+        Workshop, CHI, 2025       
+      </li>
+      <li>
+        "Silencing at Scale: AI-Generated Non-Consensual Intimate Images 
+        as a Challenge for Technological Security."{" "}
+       <span className="name">Michelle L. Ding</span> and Malika Saada Saar. 
+        To be in the Oxford Handbook of Human Security, 2026.        
       </li>
        
         </ul>
