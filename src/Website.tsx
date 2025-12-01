8888d0c319a5c98c99f60b7b563802ddcd49537a
mding16 <michelle_ding@brown.edu>
2025-12-01 10:52:28 -0500

    added

--- src/Website.tsx
+++ src/Website.tsx
@@ -7,11 +7,18 @@ export default function Website() {
         <h1>Website Details</h1>
        I built this website using React, HTML/CSS/TS on VSCode and 
        hosted it using Github Pages.
-       I used ChatGPT to write starter code for styling, React router set up, 
+       I used ChatGPT* to write starter code for styling, React router set up, 
        accessibility features, mobile responsivity, and minor debugging. No website content or personal
        information about me or my collaborators was inputted {" "}
        <a href="https://chatgpt.com/share/68f3fa85-7908-8013-a521-9b62fe6033d0" id="ref" target="_blank">
-        (see chat history here)</a>. Last updated November 2025.
+        (chat history)</a>. Last updated November 2025.
+
+        <br></br>
+        <br></br>
+        
+        <a href="https://michellelding.substack.com/p/coding-isnt-everything" target="_blank">
+     
+        *Read my thoughts on using ChatGPT to code</a>. 
 
     </main>
   );
