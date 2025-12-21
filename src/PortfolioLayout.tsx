46e315feb61233b40926fe968e6426efcf8391db
mding16 <michelle_ding@brown.edu>
2025-12-21 10:40:26 -0500

    resources

--- src/PortfolioLayout.tsx
+++ src/PortfolioLayout.tsx
@@ -5,12 +5,27 @@ import michelle from './images/michelle_ding_portrait.jpg'
 const PortfolioLayout = () => {
   return (
     <div className="portfolio-layout">
+       <div className="brownresources">
+
+                BROWN COMMUNITY RESOURCES:{" "}
+<a href="https://michellelding.substack.com/p/caring-for-yourself-and-each-other"
+              id="ref" target="_blank">
+                [Illustrated Guide]
+              </a>{" "}
+              <a href="https://docs.google.com/spreadsheets/d/1xUQaOY7KlPeC0Y4JFPUqTS_xYSLlpiWaLjTEZE6Vwvw/edit?usp=sharing"
+              id="ref" target="_blank">
+                [Full Spreadsheet]
+              </a>
+              </div>
       <h1 className="mobile-header">Michelle L. Ding</h1>
       <main className="portfolio-bio" role="main" aria-label="Biography">
+        
         <div className="bio-container">
           {/* Bio Text */}
           <div className="bio-text">
+            
             <h1 className="desktop-header">Michelle L. Ding</h1>
+            
             <p>
             Hi! I'm Michelle, a researcher and community organizer critically investigating how AI systems impact communities. 
             I am currently a Computer Science Ph.D student at Brown University
@@ -20,18 +35,19 @@ const PortfolioLayout = () => {
                     <a href="https://cntr.brown.edu/" id="ref" target="_blank">Center for Technological Responsibility, Reimagination, and Redesign (CNTR)</a> 
                     {" "} and the {" "}
                     <a href="https://discolab.cs.brown.edu/" id="ref" target="_blank">Data in Society Collective (DISCO) Lab.</a>{" "}
-            {/* I founded the {" "}
-            <a href="https://srch.cs.brown.edu" id="ref" target="_blank">Socially
-            Responsible Computing Handbook</a>, a think tank and resource for socio-technical computing education.  {" "} */}
             </p>
+        
              <div className="bio-links" aria-label="Quick Links">
+              
               <a href="mailto:michelle_ding@brown.edu" id="ref" target="_blank">Email</a>
             <a href="https://bsky.app/profile/michelleding.bsky.social" id="ref" target="_blank">Bluesky</a>
             <a href="https://www.linkedin.com/in/michelleding1/" id="ref" target="_blank">LinkedIn</a>
             <a href="https://michellelding.substack.com/" id="ref" target="_blank">Substack</a>
             <a href="https://scholar.google.com/citations?user=yk_fq5AAAAAJ&hl=en" id="ref" target="_blank">Google Scholar</a>
             </div>
-          </div>
+          
+           
+              </div>
           
           {/* Photo */}
           <img
